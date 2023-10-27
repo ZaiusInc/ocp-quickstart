0943047f108ef01bae73f7b9afc4cfb3984bba41
Pawel Zieba <pawel.zieba@optimizely.com>
2023-10-27 09:40:05 +0200

    OCP-591 improving order purchase event format (#5)
    * OCP-591: improving format of order purchase event
    
    * OCP-591: bumping version

--- src/data/DataFiles.ts
+++ src/data/DataFiles.ts
@@ -1,9 +1,17 @@
+export interface OrderItem {
+  product_id: string;
+  quantity: number;
+  price: number;
+  discount: number;
+  subtotal: number;
+}
+
 export interface OrderInfo {
   order_id: string;
-  product_id: string;
   price_total: number;
   customer_email: string;
   customer_loyalty_card_id: string;
   customer_loyalty_card_creation_date: number;
   offline_store_id: string;
+  items: OrderItem[];
 }
